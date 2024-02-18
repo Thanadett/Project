dfcaf6c4c3a8c22f2e7917aa554d4ba96938921d
Kittifirst <newfirst47@gmail.com>
2024-02-18 16:16:13 +0700

    new

--- Project1/MainForm.h
+++ Project1/MainForm.h
@@ -1,6 +1,24 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <msclr/marshal_cppstd.h>
+#include <fstream>
 
 namespace Project1 {
+	struct Medicine
+	{
+		std::string name;
+		std::string indication;
+		std::string dosage;
+		std::string frequency;
+	};
+
+	std::vector<Medicine> medicineDatabase = {
+		{"Paracetamol", "ลดไข้และแก้ปวด", "1-2 เม็ด", "ทุก 4-6 ชั่วโมง"},
+		{"Ibuprofen", "ลดอาการปวดและลดการอักเสบ", "200-400 มก.", "ทุก 4-6 ชั่วโมง"},
+		{"Cetirizine", "บรรเทาอาการแพ้", "10 มก.", "ทุก 24 ชั่วโมง"},
+		// เพิ่มยาต่อไปตามต้องการ
+	};
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -9,19 +27,34 @@ namespace Project1 {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
+
 	/// <summary>
 	/// Summary for MainForm
 	/// </summary>
 	public ref class MainForm : public System::Windows::Forms::Form
 	{
+	private:
+		MainForm^ mainFormInstance;
+
+		// ฟังก์ชันที่ตรวจสอบคำที่ตรงกับ medicineDatabase
+		std::vector<Medicine> findMatchingMedicines(const std::string& userIndication)
+		{
+			std::vector<Medicine> matchingMedicines;
+			for (const Medicine& med : medicineDatabase) {
+				if (med.indication.find(userIndication) != std::string::npos) {
+					matchingMedicines.push_back(med);
+				}
+			}
+			return matchingMedicines;
+		}
+
 	public:
 		MainForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
+			mainFormInstance = this; // กำหนด mainFormInstance เป็น object ปัจจุบัน
 		}
+
 
 	protected:
 		/// <summary>
@@ -34,6 +67,17 @@ namespace Project1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ headtx;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label2;
+
+
+
+
+	protected:
+
 
 	protected:
 
@@ -50,21 +94,122 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MainForm::typeid));
+			this->headtx = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// headtx
+			// 
+			this->headtx->AutoSize = true;
+			this->headtx->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24));
+			this->headtx->Location = System::Drawing::Point(379, 121);
+			this->headtx->Name = L"headtx";
+			this->headtx->Size = System::Drawing::Size(393, 46);
+			this->headtx->TabIndex = 1;
+			this->headtx->Text = L"Medicine Emergency";
+			this->headtx->Click += gcnew System::EventHandler(this, &MainForm::label1_Click);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(537, 379);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Search";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16));
+			this->label1->Location = System::Drawing::Point(239, 215);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(713, 31);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"คุณกำลังมองหายารักษาโรคอยู่เหรอ ถามฉันมาได้เลยฉันจะตอบคุณเอง";
+			this->label1->Click += gcnew System::EventHandler(this, &MainForm::label1_Click_1);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16));
+			this->textBox1->Location = System::Drawing::Point(340, 305);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(476, 38);
+			this->textBox1->TabIndex = 4;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MainForm::textBox1_TextChanged);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(620, 483);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(12, 17);
+			this->label2->TabIndex = 5;
+			this->label2->Text = L" ";
+			this->label2->Click += gcnew System::EventHandler(this, &MainForm::label2_Click);
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(529, 410);
+			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(1178, 617);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->headtx);
+			this->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8));
+			this->ForeColor = System::Drawing::Color::Black;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->Name = L"MainForm";
-			this->Text = L"MainForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Medicine Emergency";
 			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
+    }
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void label1_Click_1(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		// ข้อมูลที่ผู้ใช้ป้อนเข้ามา
+		std::string userIndication = msclr::interop::marshal_as<std::string>(textBox1->Text);
+
+		// ค้นหายาที่ตรงกับอาการ
+		std::vector<Medicine> matchingMedicines = findMatchingMedicines(userIndication);
+
+		// แสดงผลลัพธ์ใน MessageBox
+		if (!matchingMedicines.empty()) {
+			std::string result = "พบยาที่ตรงกับอาการ:\n\n";
+			for (const Medicine& med : matchingMedicines) {
+				result += "ชื่อยา: " + med.name + "\n";
+				result += "ใช้สำหรับ: " + med.indication + "\n";
+				result += "ขนาดยา: " + med.dosage + "\n";
+				result += "ประเภท: " + med.frequency + "\n\n";
+			}
+			MessageBox::Show(gcnew System::String(result.c_str()), "ผลลัพธ์", MessageBoxButtons::OK);
+		}
+		else {
+			MessageBox::Show("ไม่พบยาที่ตรงกับอาการ", "ผลลัพธ์", MessageBoxButtons::OK);
+		}
+	}
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}		
+	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
