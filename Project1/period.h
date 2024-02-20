4c9ef5e0723afb1de4e3dbf3676cf8d3c463e27a
Aingei <aing36334@gmail.com>
2024-02-20 12:40:01 +0700

    draft

--- Project1/period.h
+++ Project1/period.h
@@ -4,27 +4,30 @@ using namespace std;
 void displayCalendar(int year, int month)
 {
     int daysInMonth;
-    for (int i = 0; i < 12; i++)
+    if (month == 1 && month == 3 && month == 5 && month == 7 && month == 9 && month == 11)
     {
-        if (i == 1 && i == 3 && i == 5 && i == 7 && i == 9 && i == 11)
-        {
-            daysInMonth = 31;
-        }
-        if (i == 4 && i == 6 && i == 8 && i == 10)
-        {
-            daysInMonth = 30;
-        }
-        if (i == 2)
-        {
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-                daysInMonth = 29;
-            else
-                daysInMonth = 28;
-        }
-        else
-        {
-            cout << "Invalid month\n";
-        }
-        return;
+        daysInMonth = 31;
     }
+    if (month == 4 && month == 6 && month == 8 && month == 10)
+    {
+        daysInMonth = 30;
+    }
+    if (month == 2)
+    {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            daysInMonth = 29;
+        else
+            daysInMonth = 28;
+    }
+    else
+    {
+        cout << "Invalid month\n";
+    }
+    return;
+}
+
+void thatday(int day)
+{
+    cout << "today: ";
+    cin >> day;
 }
